0d52cee40c6119d8dcfd7101c0cf47f01c67bce6
Jose Albizures <albizures3601@gmail.com>
2022-07-25 07:47:34 -0600

    fix: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,3 @@
-import { search } from '../utils/github';
-
 export default function Home() {
-	return (
-		<div className="bg-green-500">
-			asdfa{' '}
-			{search({
-				stars: {
-					start: 200000,
-					end: 400000,
-				},
-			})}
-		</div>
-	);
+	return <div className="bg-green-500"></div>;
 }
